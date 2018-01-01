cd3ec562e95135285af2215a1eef0e1fc0eb5421
takmnagaya <takmnagaya@gmail.com>
2018-01-01 12:23:16 +0900

    Slackにデプロイ通知するlambda関数作成

--- deploy-notification-to-slack/handler.py
+++ deploy-notification-to-slack/handler.py
@@ -1,24 +1,54 @@
 import json
+import requests
+import os
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+class DeployEvent:
+    def __init__(self, eventTriggerName, applicationName, deploymentId, deploymentGroupName, status, errorInformation):
+        self.eventTriggerName = eventTriggerName
+        self.applicationName = applicationName
+        self.deploymentId = deploymentId
+        self.deploymentGroupName = deploymentGroupName
+        self.status = status
+        self.errorInformation = errorInformation
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    def is_failed(self):
+        return self.status == 'Failed'
 
-    return response
+    def event_message(self):
+        if self.is_failed():
+            message = """
+            @channel {0} {1}のデプロイに失敗しました
+            deploymentId: {2}
+            ErrorCode: {3}
+            ErrorMessage: {4}
+            """.format(self.applicationName, self.deploymentGroupName, self.deploymentId,
+                       self.errorInformation['ErrorCode'], self.errorInformation['ErrorMessage'])
+        else:
+            message = """
+            @channel {0} {1}のデプロイに成功しました
+            deploymentId: {2}
+            """.format(self.applicationName, self.deploymentGroupName, self.deploymentId)
+        return message
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def sns_message(event):
+    return json.loads(event['Records'][0]['Sns']['Message'])
+
+
+def webhook_icon(event):
+    if event.is_failed():
+        return ':rotating_light:'
+    else:
+        return ':white_check_mark:'
+
+
+def notify_to_slack(event, context):
+    message = sns_message(event)
+    deploy_event = DeployEvent(message['eventTriggerName'], message['applicationName'], message['deploymentId'],
+                               message['deploymentGroupName'], message['status'], message['errorInformation'])
+    requests.post(os.environ['SLACK_WEBHOOK_URL'], data=json.dumps({
+        'text': deploy_event.event_message(),
+        'link_names': 1,  # メンションを有効にする
+        'icon_emoji': webhook_icon(deploy_event),
+    }))
