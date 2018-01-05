641f5684c6bc65f54d2cda34a8ac704af9974f0f
takmnagaya <takmnagaya@gmail.com>
2018-01-05 22:40:06 +0900

    デプロイ成功の場合errorInformationは返らないためKeyerrorになっていた
    getメソッドで値を取得するように修正した。
    
    close #2

--- deploy-notification-to-slack/handler.py
+++ deploy-notification-to-slack/handler.py
@@ -52,8 +52,10 @@ def webhook_icon(event):
 def notify_to_slack(event, context):
     message = sns_message(event)
     print(message)
-    deploy_event = DeployEvent(message['eventTriggerName'], message['applicationName'], message['deploymentId'],
-                               message['deploymentGroupName'], message['status'], message['errorInformation'])
+    deploy_event = DeployEvent(message.get('eventTriggerName'), message.get('applicationName'),
+                               message.get('deploymentId'),
+                               message.get('deploymentGroupName'), message.get('status'),
+                               message.get('errorInformation'))
     requests.post(os.environ['SLACK_WEBHOOK_URL'], data=json.dumps({
         'text': deploy_event.event_message(),
         'link_names': 1,  # メンションを有効にする
